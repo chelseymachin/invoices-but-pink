17dbd75514d20edf94544303330833b52e5456aa
chelseymachin <chelseymachin@gmail.com>
2022-02-24 15:33:19 -0800

    Initial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="flex">
+      <div class="branding flex">
+          <img src="@/assets/file-invoice-dollar-solid.png" alt="">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "navigation"
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    z-index: 99;
+    flex-direction: row;
+    background-color: #440381;
+    @media(min-width: 900px) {
+        min-height: 100%;
+        min-width: 90px;
+        flex-direction: column;
+        border-radius: 0 20px 20px 0;
+    }
+
+    .branding {
+        border-radius: 0 20px 20px 0;
+        background-color: #FFA5A5;
+        justify-content: center;
+        padding: 24px;
+        @media(min-width: 900px) {
+            width: 100%;
+        }
+
+        img {
+            width: auto;
+            height: 30px;
+        }
+    }
+}
+</style>
